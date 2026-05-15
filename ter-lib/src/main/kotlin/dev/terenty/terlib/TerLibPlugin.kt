f4b261c3e9f7c19d6605ced276b1817850182a35
Terenty <galera951@gmail.com>
2026-05-15 14:13:42 +0300

    fix(ter-lib): clear singleton instance on disable

--- ter-lib/src/main/kotlin/dev/terenty/terlib/TerLibPlugin.kt
+++ ter-lib/src/main/kotlin/dev/terenty/terlib/TerLibPlugin.kt
@@ -5,12 +5,16 @@ import org.bukkit.plugin.java.JavaPlugin
 class TerLibPlugin : JavaPlugin() {
 
     companion object {
-        lateinit var instance: TerLibPlugin
+        var instance: TerLibPlugin? = null
             private set
     }
 
     override fun onEnable() {
         instance = this
         logger.info("TerLib ${pluginMeta.version} enabled")
+    }
+
+    override fun onDisable() {
+        instance = null
     }
 }
